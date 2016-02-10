3c0737b5f83543afe2f7c4e11c6812917b266333
Michael Silveira <gravitypersists@gmail.com>
2016-02-09 19:22:53 -0800

    adds some basic first tests

--- test/crudCollection.spec.js
+++ test/crudCollection.spec.js
@@ -1,7 +1,79 @@
 import expect from 'expect'
+import { createStore } from 'redux'
 
-describe('working', () => {
-  it('does', () => {
-    expect(2).toEqual(2)
+import crudCollection from '../src/crudCollection'
+import actionCreatorsFor from '../src/actionCreatorsFor'
+
+const testActions = actionCreatorsFor('test')
+
+describe('Fetching', () => {
+  const reducer = crudCollection('test')
+  const store = createStore(reducer)
+
+  describe('Initialize', () => {
+
+    it('does not inject anything before use', () => {
+      expect(store.getState().items).toEqual([])
+    })
+
+    it('has error set to null', () => {
+      expect(store.getState().error).toEqual(null)
+    })
+
+    it('has status set to "success"', () => {
+      expect(store.getState().status).toEqual('success')
+    })
+
   })
+
+  describe('Start', () => {
+
+    before(() => {
+      store.dispatch(testActions.fetchStart())
+    })
+
+    it('sets status to "pending"', () => {
+      expect(store.getState().status).toEqual('pending')
+    })
+
+  })
+
+  describe('Success', () => {
+
+    before(() => {
+      store.dispatch(testActions.fetchSuccess([{ test: 'isGood' }]))
+    })
+
+    it('sets status to "success"', () => {
+      expect(store.getState().status).toEqual('success')
+    })
+
+    it('adds the new items', () => {
+      expect(store.getState().items.length).toEqual(1)
+    })
+
+    it('adds a cid to the new item', () => {
+      expect(store.getState().items[0].cid).toEqual(0)
+    })
+
+  })
+
+  describe('Subsequent success', () => {
+
+    before(() => {
+      store.dispatch(testActions.fetchStart())
+      store.dispatch(testActions.fetchSuccess([{ test: 'anotherIsGood' }]))
+    })
+
+    it('merges the new item', () => {
+      expect(store.getState().items.length).toEqual(2)
+    })
+
+    it('adds a unique cid to the new item', () => {
+      expect(store.getState().items[1].cid).toEqual(1)
+    })
+
+  })
+
+
 })
